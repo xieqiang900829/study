7ab8f37ce030ee6863526e9866b011a38dee09c2
xieqiang900829 <1064976419@qq.com>
2019-06-11 16:30:17 +0800

    javaagent学习测试

--- src/main/java/com/javaagent/Demo.java
+++ src/main/java/com/javaagent/Demo.java
@@ -1,12 +1,18 @@
 package com.javaagent;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Demo {
 
-    public void add(){
-        List<String> list =null;
+    private int  sex ;
+    private String name;
+    public Integer add(int a,int  b){
+        List<Integer> list =new ArrayList<>();
+        list.add(a+b);
+        System.err.println("方法处理中");
+        return 100;
     }
 
     public void print(){
--- src/main/java/com/javaagent/JavaAgentDemo.java
+++ src/main/java/com/javaagent/JavaAgentDemo.java
@@ -1,11 +1,15 @@
 package com.javaagent;
 
+import com.classloader.MyClassLoader;
+import com.classloader.MyClassLoader2;
 import javassist.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
 
 public class JavaAgentDemo {
 
@@ -37,8 +41,122 @@ public class JavaAgentDemo {
                 "    }",ctClass);
         ctClass.addMethod(ctMethod);
 
+        //输出到磁盘
         byte[] bytes = ctClass.toBytecode();
         OutputStream out = new FileOutputStream("E:\\jd_gui\\class_dir\\Util.class");
         out.write(bytes);
+
+        //方法调用
+        Class c = ctClass.toClass();
+        Object obj = c.newInstance();
+        Method method = c.getMethod("print");
+        method.invoke(obj);
+    }
+
+
+    @Test
+    public void  f3() throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.javaagent.Demo");
+        CtMethod ctMethod = ctClass.getDeclaredMethod("add",new CtClass[]{CtClass.intType,CtClass.intType});
+
+        ctMethod.insertBefore("{System.out.println(\"开始执行\");}");
+        ctMethod.insertAfter("{System.out.println(\"结束执行\");}",false);
+        //方法调用
+        Class c = ctClass.toClass();
+        //toClass、writeFile 、tobytecode之后CtClass被冻结，该类已经被加载到JVM、不允许再修改
+        Object obj = c.newInstance();
+        Method method = c.getMethod("add",int.class,int.class);
+        method.invoke(obj,200,300);
+    }
+
+    @Test
+    public void f4() throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.javaagent.Demo");
+        ctClass.setName("Demo1");
+
+        CtClass ctClass1 = pool.get("com.javaagent.Demo");
+        CtClass ctClass2 = pool.get("Demo1");
+
+        Assert.assertNotNull(ctClass1);//true
+        Assert.assertNotNull(ctClass2);//true
+    }
+
+    @Test
+    public void f5() throws Exception {
+        //javassist同个Class是不能在同个ClassLoader中加载两次的。所以在输出CtClass的时候需要注意下
+        Demo demo = new Demo();
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.javaagent.Demo");
+        //ctClass.toClass();//会报错  因为同个Class是不能在同个ClassLoader中加载两次
+        ctClass.toClass(new MyClassLoader2());
+    }
+
+    @Test
+    public void f6() throws Exception {
+        System.out.println(String.class.getClassLoader());
+    }
+
+    @Test
+    public void  f7() throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.javaagent.Demo");
+        CtMethod ctMethod = ctClass.getDeclaredMethod("add",new CtClass[]{CtClass.intType,CtClass.intType});
+
+        ctMethod.insertBefore(
+                " System.out.println($0);" +
+                "System.out.println($args);" +
+                "System.out.println($class);" +
+               // "System.out.println($r);" +  //用于强制类型转换、不能直接输出
+               // "System.out.println($w);" +
+               // "System.out.println($_);" +
+                "System.out.println( $sig);" +
+                "System.out.println($1); System.out.println($2);");
+        //方法调用
+        Class c = ctClass.toClass();
+        //toClass、writeFile 、tobytecode之后CtClass被冻结，该类已经被加载到JVM、不允许再修改
+        Object obj = c.newInstance();
+        Method method = c.getMethod("add",int.class,int.class);
+        method.invoke(obj,200,300);
+    }
+
+    @Test
+    public void addCatch()throws Exception{
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.javaagent.Demo");
+        CtMethod method = ctClass.getDeclaredMethod("add",new CtClass[]{CtClass.intType,CtClass.intType});
+        CtClass exceptionType = pool.get("java.io.IOException");
+       // method.addLocalVariable("myAge",CtClass.intType);  //不生效
+        method.addCatch("{ System.out.println($e); throw $e; }",exceptionType);
+
+       ctClass.writeFile("E:\\jd_gui\\class_dir");
+    }
+
+    @Test
+    public void remove()throws Exception{
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.javaagent.Demo");
+
+        ctClass.removeField(ctClass.getDeclaredField("name"));
+        ctClass.removeMethod(ctClass.getDeclaredMethod("print"));
+
+        ctClass.writeFile("E:\\jd_gui\\class_dir");
+    }
+
+    @Test
+    public void testBeforeAndAfter()throws Exception{
+        ClassPool pool = ClassPool.getDefault();
+        CtClass ctClass = pool.get("com.javaagent.Demo");
+        CtMethod ctMethod = ctClass.getDeclaredMethod("add",new CtClass[]{CtClass.intType,CtClass.intType});
+
+        ctMethod.insertBefore("{System.out.println(\"开始执行\");}");
+        ctMethod.insertAfter("{System.out.println(\"结束执行\");}",false);
+        ctClass.writeFile("E:\\jd_gui\\class_dir");
+
+        Class c = ctClass.toClass();
+        Object obj = c.newInstance();
+        Method method = c.getMethod("add",int.class,int.class);
+        method.invoke(obj,200,300);
     }
 }
